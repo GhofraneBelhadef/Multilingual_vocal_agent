96b3c7bb42c9751c16e8dd792192eb89204d7d79
GhofraneBelhadef <ghofrane.belhadef@esprit.tn>
2025-06-25 21:47:30 +0100

    vocal response genration works

--- tts_utils.py
+++ tts_utils.py
@@ -2,6 +2,8 @@ import pyttsx3
 
 def speak_response(text):
     engine = pyttsx3.init()
-    engine.say(text)
+    engine.save_to_file(text, "audio/response.mp3")
     engine.runAndWait()
     engine.stop()
+    return 'audio/response.mp3'
+    
